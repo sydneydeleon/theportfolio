fc26e4f4993a6a1fd9c8377f970944a392a4ce79
sydneydeleon <77764699+sydneydeleon@users.noreply.github.com>
2021-05-30 23:22:05 -0400

    resume header

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -1,11 +1,33 @@
 import React from 'react';
 import './Resume.css';
+import FloralIcon from '../images/floralicon.png';
 
 function Resume() {
     return (
-        <div className="content">
-            <h1>resume</h1>
+        <div className="resume-header">
             
+            <div className="resume-title">
+                Resume
+            </div>
+
+            <div className="resume-intro">
+                Hi, I'm <span>Orpheus Designs!</span>
+                <ul><li>
+                I'm a <b>&#60; Web / &#62; designer,</b>
+                </li><li>
+                <b>&#60; Web / &#62; builder,</b>
+                </li>
+                <li>
+                <b>&#60; Web / &#62; wizard,</b>
+                </li><li>
+                <em>and more...</em>
+                </li>
+                </ul>
+            </div>
+            
+    
+
+
         </div>
     )
 }
