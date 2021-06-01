8018fb7d67cabe76ee7e01dea119581b7120270e
sydneydeleon <77764699+sydneydeleon@users.noreply.github.com>
2021-05-31 20:05:19 -0400

    new resume

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -13,123 +13,50 @@ function Resume() {
     return (
         <div className="resume-container">
             <div className="resume-header">
-                
-                <div className="resume-title">
-                    Resume
+                header
+            </div>
+
+            <div className="resume-col-container">
+
+                <div className="resume-col-one">
+                    
+                    <div className="resume-title-a">
+                        <h1>&#60; Experience /&#62;</h1>
+                    </div>
+                    heyhey<br />
+                    heyhey<br />
+                    heyhey<br />
                 </div>
 
-                <div className="resume-intro">
-                    Hi, I'm <span>Orpheus Designs!</span>
-                    <ul><li>
-                    I'm a <b>&#60; Web / &#62; designer,</b>
-                    </li><li>
-                    <b>&#60; Web / &#62; builder,</b>
-                    </li>
-                    <li>
-                    <b>&#60; Web / &#62; wizard,</b>
-                    </li><li>
-                    <em>and more...</em>
-                    </li>
-                    </ul>
+                <div className="resume-col-two">
+                    <div className="resume-title-b">
+                        <h1>&#60; Skills /&#62;</h1>
+                    </div>
+                    heyhey<br />
+                    heyhey<br />
+                    heyhey<br />
+                </div>
+
+                <div className="resume-col-four">
+                    <div className="resume-title-a">
+                        <h1>&#60; Education /&#62;</h1>
+                    </div>
+                    heyhey<br />
+                    heyhey<br />
+                    heyhey<br />
+                </div>
+
+                <div className="resume-col-five">
+                    <div className="resume-title-b">
+                        <h1>&#60; Awards /&#62;</h1>
+                    </div>
+                    heyhey<br />
+                    heyhey<br />
+                    heyhey<br />
                 </div>
 
             </div>
-
-            <div className="resume-gradient">
-            <div className="skill-title">
-                        highlighted skills
-                    </div>
-                <div className="resume-skills">
-                    <div className="skill">
-                        <div className="skill-icon">
-                            <FaIcons.FaSwatchbook/></div>
-                        <div className="skill-label">
-                            design</div>
-                    </div>
-
-                    <div className="skill">
-                        <div className="skill-icon">
-                            <FaIcons.FaLaptopCode/></div>
-                        <div className="skill-label">
-                            html</div>
-                    </div>
-
-                    <div className="skill">
-                        <div className="skill-icon">
-                            <FaIcons.FaRegFileCode/></div>
-                        <div className="skill-label">
-                            css</div>
-                    </div>
-
-                    <div className="skill">
-                        <div className="skill-icon">
-                            <FaIcons.FaPencilAlt/></div>
-                        <div className="skill-label">
-                            writing</div>
-                    </div>
-
-                    <div className="skill">
-                        <div className="skill-icon">
-                            <FaIcons.FaUserTag/></div>
-                        <div className="skill-label">
-                            branding</div>
-                    </div>
-
-                    <div className="skill">
-                        <div className="skill-icon">
-                            <DiIcons.DiPhotoshop/></div>
-                        <div className="skill-label">
-                            photoshop</div>
-                    </div>
-
-
-                    <div className="skill">
-                        <div className="skill-icon">
-                            <DiIcons.DiIllustrator/></div>
-                        <div className="skill-label">
-                            illustrator</div>
-                    </div>
-
-                    <div className="skill">
-                        <div className="skill-icon">
-                            <SiIcons.SiAdobeindesign/></div>
-                        <div className="skill-label">
-                            in design</div>
-                    </div>
-
-                    <div className="skill">
-                        <div className="skill-icon">
-                            <SiIcons.SiAdobepremiere/></div>
-                        <div className="skill-label">
-                            priemere pro</div>
-                    </div>
-
-                    <div className="skill">
-                        <div className="skill-icon">
-                            <AiIcons.AiOutlineCode/></div>
-                        <div className="skill-label">
-                            c/c++</div>
-                    </div>
-
-                    <div className="skill">
-                        <div className="skill-icon">
-                            <BiIcons.BiData/></div>
-                        <div className="skill-label">
-                            mysql</div>
-                    </div>
-
-                </div>
-
-            </div>
-
-            <div className="resume-experience">
-                    experience
-                </div>
-        
-            <div className="bottom-gradient">
-
-                
-            </div>
+            
         
         </div>
     )
