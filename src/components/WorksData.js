20a902b99a87c59fa90f17d18b1e82f4fdb35604
sydneydeleon <sydneymdeleon@gmail.com>
2021-08-13 11:53:11 -0400

    bg

--- src/components/WorksData.js
+++ src/components/WorksData.js
@@ -4,77 +4,77 @@ export const WorksData = [
         subtitle: 'website (live)',
         desc: 'A proposed redesign for the Animal Crossing: New Horizons informational wiki. (University project.)',
         path: '/sites/newhorizons',
-        pic: 'https://i.imgur.com/sf0ompT.png'
+        pic: 'https://i.imgur.com/HhjDM8H.jpg'
     },
     {
         title: 'Androids',
         subtitle: 'website (live)',
         desc: 'A sales website based in the world of video game Detroit: Become Human. (University project.)',
         path: '/sites/detroitbecomehuman',
-        pic: 'https://via.placeholder.com/280x230'
+        pic: 'https://i.imgur.com/KvtnlsH.jpg'
     },
     {
         title: 'Black Flag Dojo',
         subtitle: 'website (live)',
         desc: 'For a final project, my professor had us draw from two baskets. Whatever we got, we had to create a website for it. I got: grunge and martial arts dojo.',
         path: '/sites/blackflag',
-        pic: 'https://via.placeholder.com/280x230'
+        pic: 'https://i.imgur.com/iLUQW7F.jpg'
     },
     {
         title: 'Follett Center',
         subtitle: 'landing page mock-up',
         desc: 'My senior advanced web technologies class partnered with the Follett center to design a website for their bootcamp. I was chosen as head of design.',
         path: '/sites/follettfamily',
-        pic: 'https://i.imgur.com/Gw2YazO.png'
+        pic: 'https://i.imgur.com/em5wZAZ.png'
     },
     {
         title: 'Catharsis',
         subtitle: 'marketing campaign',
         desc: 'A marketing campaign for my poetry performance at my college.',
         path: '/graphics/catharsis',
-        pic: 'https://i.imgur.com/cZtWnHy.png'
+        pic: 'https://i.imgur.com/bHApQDq.png'
     },
     {
         title: 'Gestalt',
         subtitle: 'marketing campaign',
         desc: 'A campaign for the concept of Gestalt. It means an organized whole that is perceived as more than the sum of its parts. (University project.)',
         path: '/graphics/gestalt',
-        pic: 'https://i.imgur.com/BkTrWUD.png'
+        pic: 'https://i.imgur.com/268nKCR.png'
     },
     {
         title: 'Gravity',
         subtitle: 'menu design',
         desc: 'A bottle service menu for Gravity nightclub.',
         path: '/graphics/gravitymenu',
-        pic: 'https://i.imgur.com/yxdUGSx.png'
+        pic: 'https://i.imgur.com/O67etQe.png'
     },
     {
         title: 'Chicago Bar & Grille',
         subtitle: 'redesign mock-up',
         desc: 'A proposed redesign for Chicago Bar & Grille website.',
         path: '/mockups/chicago',
-        pic: 'https://i.imgur.com/rPaUN1Z.png'
+        pic: 'https://i.imgur.com/r4h76Tc.png'
     },
     {
         title: 'D`Andrea Salon',
         subtitle: 'website mock-up (v1)',
         desc: 'A proposed website for local Cosmetologist, Nichole D`Andrea. Version one.',
         path: '/mockups/salonv1',
-        pic: 'https://i.imgur.com/xXRdDuE.png'
+        pic: 'https://i.imgur.com/OKkpdMD.png'
     },
     {
         title: 'D`Andrea Salon',
         subtitle: 'website mock-up (v2)',
         desc: 'A proposed website for local Cosmetologist, Nichole D`Andrea. Version two.',
         path: '/mockups/salonv2',
-        pic: 'https://i.imgur.com/cCChtEP.png'
+        pic: 'https://i.imgur.com/IeuaOXk.png'
     },
     {
         title: 'Giampiero Bodino',
         subtitle: 'redesign mock-up (v1)',
         desc: 'A proposed redesign for Italian Jeweller Giampiero Bodino. Version one.',
         path: '/mockups/bodinov1',
-        pic: 'https://i.imgur.com/eZQwDyg.png'
+        pic: 'https://i.imgur.com/4OeMRZI.png'
     },
 
     {
@@ -82,13 +82,13 @@ export const WorksData = [
         subtitle: 'redesign mock-up (v2)',
         desc: 'A proposed redesign for Italian Jeweller Giampiero Bodino. Version two.',
         path: '/mockups/bodinov2',
-        pic: 'https://i.imgur.com/cr44Gyc.png'
+        pic: 'https://i.imgur.com/ZZBYIBN.png'
     },
     {
         title: 'Giampiero Bodino',
         subtitle: 'website redesign (v3)',
         desc: 'A proposed redesign for Italian Jeweller Giampiero Bodino. Version three.',
         path: '/mockups/bodinov3',
-        pic: 'https://i.imgur.com/8T6JZA3.png'
+        pic: 'https://i.imgur.com/ajL5xKv.png'
     }
 ]
